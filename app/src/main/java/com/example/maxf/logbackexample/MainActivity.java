33c6558a126a59b92dd38657d259830e304b4f41
MaxF <FilenkovMaxim@gmail.com>
2018-10-30 10:01:01 +0400

    reset logs

--- app/src/main/java/com/example/maxf/logbackexample/MainActivity.java
+++ app/src/main/java/com/example/maxf/logbackexample/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.maxf.logbackexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.core.joran.spi.JoranException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     LOG.debug("onCreate()");
     setContentView(R.layout.activity_main);
+    findViewById(R.id.resetButton).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        resetLogs();
+      }
+    });
+  }
+
+  private void resetLogs() {
+    LOG.debug("resetLogs()");
+    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+    ContextInitializer ci = new ContextInitializer(lc);
+    lc.reset();
+    try {
+      ci.autoConfig();
+    } catch (JoranException e) {
+      LOG.error("resetLogs() ci.autoConfig() fault: {}", e);
+    }
   }
 }
